979a9e1dc1a6a27dd79c67b0f616f50507028fa3
Brandon Rachelski <brandon.rachelski@vizientinc.com>
2021-07-21 10:53:29 -0500

    inital completed state

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,102 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatCardHarness} from '@angular/material/card/testing';
+import {MatButtonHarness} from '@angular/material/button/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let loader: HarnessLoader;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        MatButtonModule,
+        MatCardModule,
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component = fixture.componentInstance;
   });
 
-  it(`should have as title 'interview-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('interview-ui');
-  });
+  describe('Mat Card', () => {
+    let matCardHarness: MatCardHarness;
+    beforeEach(async () => {
+      matCardHarness = await loader.getHarness(MatCardHarness);
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('interview-ui app is running!');
+    it('should exist', () => {
+      expect(matCardHarness).toBeTruthy();
+    });
+
+    describe('Title', () => {
+      it('should ', async () => {
+        expect(await matCardHarness.getTitleText()).toEqual('Button Not Clicked')
+      });
+    });
+
+    describe('Button', () => {
+      let buttonHarness: MatButtonHarness;
+      beforeEach(async () => {
+        buttonHarness = await matCardHarness.getHarness(MatButtonHarness);
+      });
+
+      it('should exist', () => {
+        expect(buttonHarness).toBeTruthy();
+      });
+
+      it('should have correct text', async () => {
+        expect(await buttonHarness.getText()).toEqual('Click Me!');
+      });
+
+      it('should have correct color', async () => {
+        expect(await (await buttonHarness.host()).getAttribute('color')).toEqual('accent');
+      });
+
+      it('should click', async () => {
+        await buttonHarness.click();
+        expect(await matCardHarness.getTitleText()).toEqual('Button Clicked 1 Time!');
+      });
+
+      describe('When Clicking Button Multiple Times', () => {
+        [2, 3, 4, 5].forEach(testCase => {
+          it(`should show ${testCase} as count of clicks`, async () => {
+            let i = 0;
+            while (i < testCase) {
+              await buttonHarness.click();
+              i ++;
+            }
+
+            expect(await matCardHarness.getTitleText()).toEqual(`Button Clicked ${testCase} Times!`);
+            expect(await buttonHarness.isDisabled()).toEqual(false);
+          });
+        });
+
+        it('should disable button after 10 clicks', async () => {
+          let i = 0;
+          while (i < 10) {
+            await buttonHarness.click();
+            i ++;
+          }
+
+          expect(await matCardHarness.getTitleText()).toEqual('Button Clicked 10 Times!');
+          expect(await buttonHarness.isDisabled()).toEqual(true);
+        });
+      });
+    });
   });
 });
